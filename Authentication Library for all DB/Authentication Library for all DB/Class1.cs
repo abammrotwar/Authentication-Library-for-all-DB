1f26dd5ec75a956fd12b91a2e03b9d5af1e729e1
abammrotwar <abammrotwar@gmail.com>
2015-04-04 22:23:41 +0530

    first commit

---
+++ Authentication Library for all DB/Authentication Library for all DB/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Authentication_Library_for_all_DB
+{
+    public class DatabaseCredentials
+    {
+        public static void DatabaseCredentials(string DBName, string ConnectionString)
+        {
+        }
+    }
+}
