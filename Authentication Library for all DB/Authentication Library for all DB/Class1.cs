4fed77ed3a3462e3da50d405261da0cbfea97b07
abammrotwar <abammrotwar@gmail.com>
2015-04-05 09:55:55 +0530

    return value

--- Authentication Library for all DB/Authentication Library for all DB/Class1.cs
+++ Authentication Library for all DB/Authentication Library for all DB/Class1.cs
@@ -14,8 +14,9 @@ namespace Authentication_Library_for_all_DB
         /// <param name="ConnectionString">Connection String for connecting database</param>
         /// <param name="ReturnValue">1 for the entire row, 2 for only id. By default it will pass 2</param>
         /// <param name="DBValue">For SQL Server pass 1, For MySQL pass 2, For Oracle Pass 3, By default we assume that you are using SQL Server </param>
-        public static void DatabaseCredentials(string DBName, string ConnectionString,int ReturnValue = 2, int DBValue = 1)
+        public static Object DatabaseCredentials(string DBName, string ConnectionString,int ReturnValue = 2, int DBValue = 1)
         {
+            object retunobject ="Dummy value";
             DBName = DBName.ToLower();
             switch (DBValue)
             {
@@ -28,6 +29,7 @@ namespace Authentication_Library_for_all_DB
                 default:
                     break;
             }
+            return retunobject;
         }
 
         private object _MSSQLServerConnection()
