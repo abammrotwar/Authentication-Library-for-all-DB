c985c0d4747a0b42451d0c41b0cef5da613b2951
abammrotwar <abammrotwar@gmail.com>
2015-04-05 14:18:00 +0530

    SQL Server implementation part 1

--- Authentication Library for all DB/Authentication Library for all DB/Class1.cs
+++ Authentication Library for all DB/Authentication Library for all DB/Class1.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
 
 namespace Authentication_Library_for_all_DB
 {
@@ -12,28 +17,50 @@ namespace Authentication_Library_for_all_DB
         /// </summary>
         /// <param name="DBName">Name of the database like MySQL, SQL Server, Oracle and any other</param>
         /// <param name="ConnectionString">Connection String for connecting database</param>
+        /// <param name="query">query</param>
         /// <param name="ReturnValue">1 for the entire row, 2 for only id. By default it will pass 2</param>
         /// <param name="DBValue">For SQL Server pass 1, For MySQL pass 2, For Oracle Pass 3, By default we assume that you are using SQL Server </param>
-        public static Object DatabaseCredentials(string DBName, string ConnectionString,int ReturnValue = 2, int DBValue = 1)
+        public static Object DatabaseCredentials(string DBName, string ConnectionString,string query, int ReturnValue = 2, int DBValue = 1)
         {
-            object retunobject ="Dummy value";
+            Authentication_Library_for_all_DB.DatabaseCredentials ds = new Authentication_Library_for_all_DB.DatabaseCredentials();
+            object retunobject = "Dummy value";
             DBName = DBName.ToLower();
-            switch (DBValue)
+            try
             {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
+                switch (DBValue)
+                {
+                    case 1:
+                        retunobject = ds._MSSQLServerConnection(ConnectionString, ReturnValue, query);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
             }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException(ex.Message);
+            }
+            
             return retunobject;
         }
 
-        private object _MSSQLServerConnection()
+        private object _MSSQLServerConnection(string ConnectionString, int ReturnValue, string query)
         {
+            if (ReturnValue == 1)
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                SqlCommand cmd = new SqlCommand(query,con);
+            }
+            else if (ReturnValue == 2)
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                SqlCommand cmd = new SqlCommand(query, con);
+            }
             return true;
         }
     }
